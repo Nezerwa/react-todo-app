9d32bac80f09511ac47832059fa6c7aa92c6ff46
Nezerwa <eligrand2000@gmail.com>
2023-03-01 15:05:40 +0200

    modify about

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,50 +11,40 @@ function About() {
           <Hero title="about me." description="Learn more about me" />
           <div className="details">
             <p>
-              My name is James Serengia from Nairobi Kenya. I am a software
+              My name is Nezerwa Eligrand from Kigali Rwanda. I am a software
               Enginner mostly specializing in MERN stack, Ruby and Ruby on Rails
-              and Next Js. I also work with React Native and Typescript.
+              and Next Js.
             </p>
             <div className="contacts">
               <ul>
                 <li>
                   <BsGithub className="icon" /> Github:{" "}
                   <a
-                    href="https://github.com/serengia"
+                    href="https://github.com/Nezerwa"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    github.com/serengia
+                    github.com/Nezerwa
                   </a>
                 </li>
                 <li>
                   <BsLinkedin className="icon" /> LinkedIn:{" "}
                   <a
-                    href="https://linkedin.com/in/james-serengia"
+                    href="https://linkedin.com/in/eligrad-nezerwa"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    linkedin.com/in/james-serengia
+                    linkedin.com/in/eligrand-Nezerwa
                   </a>
                 </li>
                 <li>
                   <BsGlobe className="icon" /> Website 1:{" "}
                   <a
-                    href="https://sawabox.com"
+                    href="https://nezerwa.github.io/Portfolio-Project/"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    sawabox.com
-                  </a>
-                </li>
-                <li>
-                  <BsGlobe className="icon" /> Website 2:{" "}
-                  <a
-                    href="https://jamesserengia.com"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    jamesserengia.com
+                    nezerwa.github.igo/Portfolio-project/
                   </a>
                 </li>
               </ul>
